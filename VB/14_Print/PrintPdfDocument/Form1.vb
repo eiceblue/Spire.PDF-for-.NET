5cee5baaf89731472b13f6814e3c01a391581073
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-03-20 15:34:50 +0800

    Add files via upload

---
+++ VB/14_Print/PrintPdfDocument/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports Spire.Pdf
+Imports System.ComponentModel
+Imports System.Text
+
+Namespace PrintPdfDocument
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a document
+			Dim doc As New PdfDocument()
+
+			'Load file
+			doc.LoadFromFile("..\..\..\..\..\..\Data\PrintPdfDocument.pdf")
+
+			'Set the printer and select the pages you want to print
+			Dim dialogPrint As New PrintDialog()
+			dialogPrint.AllowPrintToFile = True
+			dialogPrint.AllowSomePages = True
+			dialogPrint.PrinterSettings.FromPage = 1
+			dialogPrint.PrinterSettings.ToPage = doc.Pages.Count
+
+            If dialogPrint.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                'print
+                doc.PrintSettings.SelectPageRange(dialogPrint.PrinterSettings.FromPage, dialogPrint.PrinterSettings.ToPage)
+                doc.PrintSettings.PrinterName = dialogPrint.PrinterSettings.PrinterName
+                doc.Print()
+            End If
+		End Sub
+	End Class
+End Namespace
---
+++ VB/14_Print/SplitPage/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Spire.Pdf
+Imports Spire.Pdf.Graphics
+
+Namespace SplitPage
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a pdf document
+			Dim doc As New PdfDocument()
+			'Load a file
+			doc.LoadFromFile("..\..\..\..\..\..\Data\SplitPage.pdf")
+			'Indicating whether the page is printed in landscape or portrait orientation.
+			doc.PrintSettings.Landscape = False
+			'Set print page range
+			doc.PrintSettings.SelectPageRange(1, 1)
+			'Select split page to multiple paper layout
+			doc.PrintSettings.SelectSplitPageLayout()
+			'Print document
+			doc.Print()
+			'Close the document
+			doc.Close()
+		End Sub
+	End Class
+End Namespace
