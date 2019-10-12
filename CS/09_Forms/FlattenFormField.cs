18a4d3f12666e92dbf4ea8cb6c2cd0589604bd82
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:56:44 +0800

    Create FlattenFormField.cs

---
+++ CS/09_Forms/FlattenFormField.cs
@@ -0,0 +1,47 @@
+﻿using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FlattenFormField
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Pdf file path
+            string input = @"..\..\..\..\..\..\Data\FlattenFormField.pdf";
+
+            //Open pdf document
+            PdfDocument doc = new PdfDocument();
+	    doc.LoadFromFile(input);
+            //Flatten form fields
+            doc.Form.IsFlatten = true;
+
+            string output = "FlattenFormField.pdf";
+
+            //Save pdf document
+            doc.SaveToFile(output);
+
+            //Launch the pdf file
+            PDFDocumentViewer(output);
+        }
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
