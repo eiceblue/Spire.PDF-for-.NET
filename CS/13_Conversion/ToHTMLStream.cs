e2c93da651d5deaf3cf5fd52482c1543b1e263ed
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 16:25:52 +0800

    Create ToHTMLStream.cs

---
+++ CS/13_Conversion/ToHTMLStream.cs
@@ -0,0 +1,33 @@
+﻿using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToHTMLStream
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a pdf document.
+            PdfDocument pdf = new PdfDocument();
+            //Load file from disk
+            pdf.LoadFromFile(@"..\..\..\..\..\..\..\Data\SampleB_1.pdf");
+
+            MemoryStream ms = new MemoryStream();
+            //Save to HTML stream
+            pdf.SaveToStream(ms, FileFormat.HTML);
+        }
+    }
+}
