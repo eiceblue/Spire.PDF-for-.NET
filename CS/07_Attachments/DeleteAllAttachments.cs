56923a9d612446269799bdf5fc9a744146948fab
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:42:59 +0800

    Create DeleteAllAttachments.cs

---
+++ CS/07_Attachments/DeleteAllAttachments.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.Attachments;
+using System.IO;
+
+namespace DeleteAllAttachments
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = @"..\..\..\..\..\..\Data\DeleteAllAttachments.pdf";
+
+            //Open pdf document
+            PdfDocument doc = new PdfDocument();
+	    doc.LoadFromFile(input);
+
+            //Get all attachments
+            PdfAttachmentCollection attachments = doc.Attachments;
+
+            //Delete all attachments
+            attachments.Clear();
+
+            string output = "DeleteAllAttachments.pdf";
+
+            //Save pdf document
+            doc.SaveToFile(output);
+
+            //Launch the Pdf file
+            PDFDocumentViewer(output);
+        }
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
