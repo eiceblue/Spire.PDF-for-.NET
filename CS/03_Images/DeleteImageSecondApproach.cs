84b3b540b3b34ac53b2589a463636beb773cef04
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:12:38 +0800

    Create DeleteImageSecondApproach.cs

---
+++ CS/03_Images/DeleteImageSecondApproach.cs
@@ -0,0 +1,49 @@
+﻿using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DeleteImageSecondApproach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a pdf document
+            PdfDocument doc = new PdfDocument();
+            
+            //Load the document from disk
+            doc.LoadFromFile(@"..\..\..\..\..\..\Data\DeleteImage.pdf");
+
+            //Get the first page
+            PdfPageBase page = doc.Pages[0];
+
+            //Delete the first image on the page
+            page.DeleteImage(0);
+
+            //Save the document
+            doc.SaveToFile("Output.pdf");
+
+            //Launch the Pdf file
+            PDFDocumentViewer("Output.pdf");
+        }
+
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
