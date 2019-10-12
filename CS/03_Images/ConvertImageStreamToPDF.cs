3d59008c7fa263f2b6dda9b6ebb967d2cc37ec41
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:12:27 +0800

    Create ConvertImageStreamToPDF.cs

---
+++ CS/03_Images/ConvertImageStreamToPDF.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+
+
+namespace ConvertImageStreamToPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a pdf document with a section and page added.
+            PdfDocument pdf = new PdfDocument();
+            PdfSection section = pdf.Sections.Add();
+            PdfPageBase page = section.Pages.Add();
+
+            // Create a FileStream object to read the imag file
+            FileStream fs = File.OpenRead(@"..\..\..\..\..\..\Data\Water.jpg");
+            // Read the image into Byte array
+            byte[] data = new byte[fs.Length];
+            fs.Read(data, 0, data.Length);
+            // Create a MemoryStream object from image Byte array
+            MemoryStream ms = new MemoryStream(data);
+            // Specify the image source as MemoryStream
+            PdfImage image = PdfImage.FromStream(ms);
+
+            //Set image display location and size in PDF
+            //Calculate rate
+            float widthFitRate = image.PhysicalDimension.Width / page.Canvas.ClientSize.Width;
+            float heightFitRate = image.PhysicalDimension.Height / page.Canvas.ClientSize.Height;
+            float fitRate = Math.Max(widthFitRate, heightFitRate);
+            //Calculate the size of image 
+            float fitWidth = image.PhysicalDimension.Width / fitRate;
+            float fitHeight = image.PhysicalDimension.Height / fitRate;
+            //Draw image
+            page.Canvas.DrawImage(image, 0, 30, fitWidth, fitHeight);
+
+            //save and launch the file
+            string output = "ConvertImageStreamToPDF.pdf";
+            pdf.SaveToFile(output);
+            System.Diagnostics.Process.Start(output);
+        }
+    }
+}
