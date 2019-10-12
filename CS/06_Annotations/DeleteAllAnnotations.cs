fee27f729de5502de5b8b9f2f87edfad3cde3133
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:40:11 +0800

    Create DeleteAllAnnotations.cs

---
+++ CS/06_Annotations/DeleteAllAnnotations.cs
@@ -0,0 +1,48 @@
+﻿using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeleteAllAnnotations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new PDF document.
+            PdfDocument document = new PdfDocument();
+
+            //Load the file from disk
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Pdf_3.pdf");
+
+            //Remove all annotations
+            document.Pages[0].AnnotationsWidget.Clear();
+
+            String result = "DeleteAllAnnotations_out.pdf";
+
+            //Save the document
+            document.SaveToFile(result);
+            //Launch the Pdf file
+            PDFDocumentViewer(result);
+        }
+        private void PDFDocumentViewer(string filename)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(filename);
+            }
+            catch { }
+        }
+    }
+}
