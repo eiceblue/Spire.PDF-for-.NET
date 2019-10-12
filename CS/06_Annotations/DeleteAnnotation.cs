84f9160235b179828dee2606618faf38e221e613
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:40:14 +0800

    Create DeleteAnnotation.cs

---
+++ CS/06_Annotations/DeleteAnnotation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+namespace DeleteAnnotation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = @"..\..\..\..\..\..\Data\DeleteAnnotation.pdf";
+
+            //Open pdf document
+            PdfDocument doc = new PdfDocument();
+	    doc.LoadFromFile(input);
+
+            //Remove the first annotation
+            doc.Pages[0].AnnotationsWidget.RemoveAt(0);
+
+            string output = "DeleteAnnotation.pdf";
+
+            //Save pdf document
+            doc.SaveToFile(output);
+
+            //Launch the Pdf file
+            PDFDocumentViewer(output);
+        }
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
