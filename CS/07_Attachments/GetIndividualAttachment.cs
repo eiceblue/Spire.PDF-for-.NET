8b871d1b7cf97522988e2f3f50743bde418d7495
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:43:05 +0800

    Create GetIndividualAttachment.cs

---
+++ CS/07_Attachments/GetIndividualAttachment.cs
@@ -0,0 +1,41 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Attachments;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetIndividualAttachment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new PDF document.
+            PdfDocument pdf = new PdfDocument();
+
+            //Load the file from disk.
+            pdf.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Pdf_2.pdf");
+
+            //Get a collection of attachments on the PDF document.
+            PdfAttachmentCollection collection = pdf.Attachments;
+
+            //Get the second attachment in PDF file.
+            PdfAttachment attachment = collection[1];
+
+            //Save the second attachment to the file.
+            File.WriteAllBytes(attachment.FileName, attachment.Data);
+        }
+    }
+}
