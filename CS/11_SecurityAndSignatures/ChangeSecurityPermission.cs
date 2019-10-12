d232f8e2201a562ea760012df3d64fc01714976b
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 16:23:53 +0800

    Create ChangeSecurityPermission.cs

---
+++ CS/11_SecurityAndSignatures/ChangeSecurityPermission.cs
@@ -0,0 +1,35 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChangeSecurityPermission
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create and load a pdf document
+            PdfDocument pdf = new PdfDocument();
+            pdf.LoadFromFile(@"..\..\..\..\..\..\Data\ChangeSecurityPermission.pdf");
+
+            //Set an owner password, enable the permissions of Printing and Copying, set encryption level
+            pdf.Security.Encrypt("", "test  ", PdfPermissionsFlags.FillFields | PdfPermissionsFlags.FullQualityPrint,PdfEncryptionKeySize.Key256Bit);
+
+            //Save and launch
+            pdf.SaveToFile("SecurityPermission.pdf");
+            System.Diagnostics.Process.Start("SecurityPermission.pdf");
+
+        }
+    }
+}
