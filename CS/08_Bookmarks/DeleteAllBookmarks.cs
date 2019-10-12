7f2c62d2976d57184331632f2adfec62be34de06
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:46:04 +0800

    Create DeleteAllBookmarks.cs

---
+++ CS/08_Bookmarks/DeleteAllBookmarks.cs
@@ -0,0 +1,48 @@
+﻿using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeleteAllBookmarks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new Pdf document.
+            PdfDocument document = new PdfDocument();
+
+            //Load the file from disk.
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Pdf_1.pdf");
+
+            //Remove all bookmarks.
+            document.Bookmarks.Clear();
+
+            String result = "DeleteAllBookmarks_out.pdf";
+
+            //Save the document
+            document.SaveToFile(result);
+            //Launch the Pdf file
+            PDFDocumentViewer(result);
+        }
+        private void PDFDocumentViewer(string filename)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(filename);
+            }
+            catch { }
+        }
+    }
+}
