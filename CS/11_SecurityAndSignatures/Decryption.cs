907bf103da900d9cee88f812a63d1c53a6b3f8f6
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 16:23:56 +0800

    Create Decryption.cs

---
+++ CS/11_SecurityAndSignatures/Decryption.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using Spire.Pdf.Security;
+
+
+namespace Decryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load a pdf document
+            String encryptedPdf = @"..\..\..\..\..\..\Data\Decryption.pdf";
+            PdfDocument doc = new PdfDocument();
+
+            //Open the document
+            doc.LoadFromFile(encryptedPdf, "test");
+
+            //Decrypt the document
+            doc.Security.Encrypt("", "", PdfPermissionsFlags.Default, PdfEncryptionKeySize.Key256Bit, "test");
+
+            //Save Pdf file
+            doc.SaveToFile("Decryption.pdf", FileFormat.PDF);
+
+            DocumentViewer("Decryption.pdf");
+        }
+
+        private void DocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
