2294e743907090490577a9347bf5afdd4871ec51
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 16:25:48 +0800

    Create ToDocx.cs

---
+++ CS/13_Conversion/ToDocx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+namespace ToDocx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load a pdf document
+            String file = @"..\..\..\..\..\..\Data\ToDocx.pdf";
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(file);;
+
+            //Convert to docx file.
+            doc.SaveToFile("ToDocx.docx", FileFormat.DOCX);
+            doc.Close();
+
+            //Launch the file.
+            System.Diagnostics.Process.Start("ToDocx.docx");
+        }
+    }
+}
