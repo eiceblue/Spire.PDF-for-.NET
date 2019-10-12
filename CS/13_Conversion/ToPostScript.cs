bdb9014ce18ade917e40d30775a629a75b9335df
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 16:25:33 +0800

    Create ToPostScript.cs

---
+++ CS/13_Conversion/ToPostScript.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+namespace ToPostScript
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input =  @"..\..\..\..\..\..\Data\ToPostScript.pdf";
+
+            //Load a PDF document
+            PdfDocument document = new PdfDocument();
+            document.LoadFromFile(input);
+
+            //Save to PostScript
+            string output = "toPostScript_result.ps";
+            document.SaveToFile(output, FileFormat.POSTSCRIPT);
+
+            //Launch the file
+            PDFDocumentViewer(output);
+        }
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+            
+        }
+    }
+}
