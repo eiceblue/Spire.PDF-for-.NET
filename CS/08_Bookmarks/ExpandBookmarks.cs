3d1b3bc795f009d0221bbd12da2511329ab7ebc7
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:46:09 +0800

    Create ExpandBookmarks.cs

---
+++ CS/08_Bookmarks/ExpandBookmarks.cs
@@ -0,0 +1,48 @@
+﻿using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpandBookmarks
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new PDF document.
+            PdfDocument doc = new PdfDocument();
+
+            //Load the file from disk.
+            doc.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Pdf_1.pdf");
+
+            //Set BookMarkExpandOrCollapse as true to expand the bookmarks.
+            doc.ViewerPreferences.BookMarkExpandOrCollapse = true;
+
+            String result = "ExpandBookmarks_out.pdf";
+
+            //Save the document
+            doc.SaveToFile(result);
+            //Launch the Pdf file
+            PDFDocumentViewer(result);
+        }
+        private void PDFDocumentViewer(string filename)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(filename);
+            }
+            catch { }
+        }
+    }
+}
