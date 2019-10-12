b065f127e11f38746b2369e22d12cc8464b9c2ae
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:40:17 +0800

    Create GetParticularAnnotationInfo.cs

---
+++ CS/06_Annotations/GetParticularAnnotationInfo.cs
@@ -0,0 +1,65 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetParticularAnnotationInfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new PDF document.
+            PdfDocument pdf = new PdfDocument();
+
+            //Load the file from disk.
+            pdf.LoadFromFile(@"..\..\..\..\..\..\Data\Template_Pdf_3.pdf");
+
+            //Get the annotation collection from the document.
+            PdfAnnotationCollection annotations = pdf.Pages[0].AnnotationsWidget;
+
+            //Get particular annotation information from the document.
+            StringBuilder content = new StringBuilder();
+            if (annotations[0] is PdfTextAnnotationWidget)
+            {
+                PdfTextAnnotationWidget textAnnotation = annotations[0] as PdfTextAnnotationWidget;
+                content.AppendLine("Annotation text: " + textAnnotation.Text);
+                content.AppendLine("Annotation ModifiedDate: " + textAnnotation.ModifiedDate.ToString());
+                content.AppendLine("Annotation author: " + textAnnotation.Author);
+                content.AppendLine("Annotation Name: " + textAnnotation.Name);
+            }
+          
+
+            String result = "GetParticularAnnotationInfo_out.txt";
+
+
+            //Save to file.
+            File.WriteAllText(result, content.ToString());
+            
+            //Launch the file.
+            DocumentViewer(result);
+        }
+
+        private void DocumentViewer(string filename)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(filename);
+            }
+            catch { }
+        }
+    }
+}
