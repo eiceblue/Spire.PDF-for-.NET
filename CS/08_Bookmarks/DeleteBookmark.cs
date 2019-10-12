98184487bc5b42e968a57263de8d0cc43ca846ce
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:46:07 +0800

    Create DeleteBookmark.cs

---
+++ CS/08_Bookmarks/DeleteBookmark.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+namespace DeleteBookmark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load a pdf document
+            string input = @"..\..\..\..\..\..\Data\DeleteBookmark.pdf";
+            PdfDocument doc = new PdfDocument();
+	    doc.LoadFromFile(input);
+
+            //Delete the first bookmark
+            doc.Bookmarks.RemoveAt(0);
+			
+            //Save the pdf document
+            string output = "DeleteBookmark.pdf";
+            doc.SaveToFile(output);
+
+            //Launch the file
+            PDFDocumentViewer(output);
+        }
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+
+    }
+}
