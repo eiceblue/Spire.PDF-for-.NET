131c0aefed6b88ccb449aa1885f8bf4fb8b496ff
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:12:30 +0800

    Create ConvertImageToPDF.cs

---
+++ CS/03_Images/ConvertImageToPDF.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+
+namespace ConvertImageToPDF
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create a pdf document with a section and page added.
+            PdfDocument pdf = new PdfDocument();
+            PdfSection section = pdf.Sections.Add();
+            PdfPageBase page = pdf.Pages.Add();
+
+            //Load a tiff image from system
+            PdfImage image = PdfImage.FromFile(@"..\..\..\..\..\..\Data\Water.jpg");
+            //Set image display location and size in PDF
+            //Calculate rate
+            float widthFitRate = image.PhysicalDimension.Width / page.Canvas.ClientSize.Width;
+            float heightFitRate = image.PhysicalDimension.Height / page.Canvas.ClientSize.Height;
+            float fitRate = Math.Max(widthFitRate, heightFitRate);
+            //Calculate the size of image 
+            float fitWidth = image.PhysicalDimension.Width / fitRate;
+            float fitHeight = image.PhysicalDimension.Height / fitRate;
+            //Draw image
+            page.Canvas.DrawImage(image, 0, 30, fitWidth, fitHeight);
+
+           string output = "ConvertImageToPDF-result.pdf";
+
+            pdf.SaveToFile(output);
+            pdf.Close();
+
+            //Launch the Pdf file
+            PDFDocumentViewer(output);
+        }
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        } 
+
+    }
+}
