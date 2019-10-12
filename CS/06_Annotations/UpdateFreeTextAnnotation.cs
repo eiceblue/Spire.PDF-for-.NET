828f51b5df7daf6f21fd711cc68eec18c427ddd4
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-12 15:40:23 +0800

    Create UpdateFreeTextAnnotation.cs

---
+++ CS/06_Annotations/UpdateFreeTextAnnotation.cs
@@ -0,0 +1,57 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Annotations;
+using Spire.Pdf.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UpdateFreeTextAnnotation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new PDF document.
+            PdfDocument pdf = new PdfDocument();
+
+            //Load the file from disk.
+            pdf.LoadFromFile(@"..\..\..\..\..\..\Data\UpdateFreeTextAnnotation.pdf");
+
+            //Get the annotation Collection from the document.
+            PdfAnnotationCollection annotations = pdf.Pages[0].AnnotationsWidget;
+
+            //Update free text annotation.
+            foreach(PdfFreeTextAnnotationWidget annotaion in annotations)
+            {
+                annotaion.Color = Color.YellowGreen;     
+             
+            }
+
+            String result = "UpdateFreeTextAnnotation_out.pdf";
+
+            //Save the document
+            pdf.SaveToFile(result);
+            //Launch the Pdf file
+            PDFDocumentViewer(result);
+        }
+        private void PDFDocumentViewer(string filename)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(filename);
+            }
+            catch { }
+        }
+    }
+}
