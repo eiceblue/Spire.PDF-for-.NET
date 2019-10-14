685508b484f720a907e0eb485e642f1526fd518f
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-10-14 09:57:08 +0800

    Create RemoveOpenAction.cs

---
+++ CS/12_LinksAndActions/RemoveOpenAction.cs
@@ -0,0 +1,52 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Actions;
+using Spire.Pdf.Annotations;
+using Spire.Pdf.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveOpenAction
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a pdf document
+            PdfDocument document = new PdfDocument();
+
+            //Load an existing pdf from disk
+            document.LoadFromFile(@"..\..\..\..\..\..\Data\OpenAction.pdf");
+
+            //Remove action
+            document.AfterOpenAction = null;
+
+            String result = "Output.pdf";
+            //Save the document
+            document.SaveToFile(result);
+
+            //Launch the Pdf file
+            PDFDocumentViewer(result);
+        }
+
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
