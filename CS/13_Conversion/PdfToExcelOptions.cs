2a524e782454a00c31ddbe8d24b73d4c91059a46
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-07 18:23:11 +0800

    Create PdfToExcelOptions.cs

---
+++ CS/13_Conversion/PdfToExcelOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using Spire.Pdf;
+
+namespace PdfToExcelOptions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {   //Load a pdf document
+            PdfDocument pdf = new PdfDocument();
+            string PDFFile = @"..\..\..\..\..\..\Data\PdfToXlsxOptions.pdf";
+            pdf.LoadFromFile(PDFFile);
+
+            pdf.ConvertOptions.SetPdfToXlsxOptions(PdfToXlsxLayout.Line);
+            string ExcelFile = "PdfToXlsxOptions_results.xlsx";
+            //Save the pdf file to excel
+            pdf.SaveToFile(ExcelFile, FileFormat.XLSX);
+            //View the excel document
+            FileViewer(ExcelFile);
+        }
+
+        private void FileViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
