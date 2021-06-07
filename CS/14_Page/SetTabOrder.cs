2eba688644a455c51fdbc1459f79adb29b1f4df6
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-07 18:24:26 +0800

    Create SetTabOrder.cs

---
+++ CS/14_Page/SetTabOrder.cs
@@ -0,0 +1,41 @@
+﻿using Spire.Pdf;
+using System;
+using System.Windows.Forms;
+
+namespace SetTabOrder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load old PDF from disk.
+            PdfDocument pdf = new PdfDocument();
+            pdf.LoadFromFile(@"..\..\..\..\..\..\Data\SetTabOrder.pdf");
+
+            //Set using document structure
+            pdf.FileInfo.IncrementalUpdate = false;
+            PdfPageBase page = pdf.Pages[0];
+            page.SetTabOrder(TabOrder.Structure);
+
+            //Save the file
+            String result = "SetTabOrder_output.pdf";
+            pdf.SaveToFile(result);
+
+            //Launch the file.
+            DocumentViewer(result);
+        }
+        private void DocumentViewer(string filename)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(filename);
+            }
+            catch { }
+        }
+    }
+}
