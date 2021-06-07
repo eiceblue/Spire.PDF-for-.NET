1fcb20e7057ae98174e6507a4c4706eb98b45527
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-07 18:24:44 +0800

    Create CustomDocumentProperties.cs

---
+++ CS/15_Document/CustomDocumentProperties.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+using System.IO;
+
+namespace CustomDocumentProperties
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String input = "..\\..\\..\\..\\..\\..\\Data\\CustomDocumentProperties.pdf";
+            PdfDocument doc = new PdfDocument();
+            //Load a pdf file from disk
+            doc.LoadFromFile(input);
+
+            //Custom document properties
+            doc.DocumentInformation.SetCustomerDefined("Company", "E-iceblue");
+            doc.DocumentInformation.SetCustomerDefined("Component", "Spire.PDF for .NET");
+            doc.DocumentInformation.SetCustomerDefined("Name", "Daisy");
+            doc.DocumentInformation.SetCustomerDefined("Team", "SalesTeam");
+            
+            //Save to file
+            String result = "CustomDocumentProperties_out.pdf";
+            doc.SaveToFile(result, FileFormat.PDF);
+
+            //Launch the file
+            DocumentViewer(result);
+        }
+
+        private void DocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
