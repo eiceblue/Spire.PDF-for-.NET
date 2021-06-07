1344f189e3a2d80fbe125410910fa4e0f314a262
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-07 18:23:44 +0800

    Create SVGToPDF.cs

---
+++ CS/13_Conversion/SVGToPDF.cs
@@ -0,0 +1,48 @@
+﻿using Spire.Pdf;
+using Spire.Pdf.Annotations;
+using Spire.Pdf.Graphics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SVGToPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Create a new PDF document.
+            PdfDocument doc = new PdfDocument();
+
+            //Load the file from disk.
+            doc.LoadFromSvg(@"..\..\..\..\..\..\Data\template.svg");
+
+            //Save the document
+            String result = "SVgToPDF_out.pdf";
+            doc.SaveToFile(result);
+
+            //Launch the Pdf file
+            PDFDocumentViewer(result);
+        }
+
+        private void PDFDocumentViewer(string filename)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(filename);
+            }
+            catch { }
+        }
+    }
+}
