aae5d0d8fd4c85c2ef10d132a7d81c7ad0c6e10a
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-07 18:18:25 +0800

    Create FindAndHighlightText.cs

---
+++ CS/02_Text/FindAndHighlightText.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Spire.Pdf;
+using Spire.Pdf.General.Find;
+
+namespace FindAndHighlightText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Load the document from disk
+            PdfDocument pdf = new PdfDocument();			
+            pdf.LoadFromFile(@"..\..\..\..\..\..\Data\FindAndHighlightText.pdf");
+            PdfTextFind[] result = null;
+            foreach (PdfPageBase page in pdf.Pages)
+            {
+                //Find text
+                result = page.FindText("science",TextFindParameter.None).Finds;
+                foreach (PdfTextFind find in result)
+                {
+                    //Highlight searched text
+                    find.ApplyHighLight();
+                }
+            }
+
+            string output = "FindAndHighlightText_out.pdf";
+            //Save the document
+            pdf.SaveToFile(output, FileFormat.PDF);
+
+            //Launch the result file
+            PDFDocumentViewer(output);
+        }
+
+        private void PDFDocumentViewer(string fileName)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(fileName);
+            }
+            catch { }
+        }
+    }
+}
