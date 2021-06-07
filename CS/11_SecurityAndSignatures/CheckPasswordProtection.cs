8b3e1655ec683d645497bff1ef8a4c3ed3f2a942
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-07 18:22:32 +0800

    Create CheckPasswordProtection.cs

---
+++ CS/11_SecurityAndSignatures/CheckPasswordProtection.cs
@@ -0,0 +1,23 @@
+﻿
+using Spire.Pdf;
+using System;
+using System.Windows.Forms;
+
+namespace CheckPasswordProtection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Check whether the input pdf document is password protected.
+            bool isProtected =PdfDocument.IsPasswordProtected(@"..\..\..\..\..\..\Data\CheckPasswordProtection.pdf");
+            //Show the result by message box
+            MessageBox.Show("The pdf is " + (isProtected ? "password " : "not password ") +"protected!");
+        }  
+    }
+}
