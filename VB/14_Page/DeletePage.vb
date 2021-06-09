cb48c18a6cac4e4f49e370bae6b0dc21761cc952
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:32:16 +0800

    Create DeletePage.vb

---
+++ VB/14_Page/DeletePage.vb
@@ -0,0 +1,36 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Spire.Pdf
+Namespace DeletePage
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Load the document from disk
+			Dim input As String = "..\..\..\..\..\..\Data\DeletePage.pdf"
+			Dim doc As New PdfDocument()
+			doc.LoadFromFile(input)
+
+			'Delete the fifth page
+			doc.Pages.RemoveAt(4)
+
+			Dim output As String = "DeletePage.pdf"
+
+			'Save the document
+			doc.SaveToFile(output)
+			doc.Close()
+
+			'Launch the Pdf file
+			PDFDocumentViewer(output)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
