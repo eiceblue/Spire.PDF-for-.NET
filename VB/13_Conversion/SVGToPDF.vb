2e0aedf6a444c5b5a70d297dc0215cf2d86979e8
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:30:31 +0800

    Create SVGToPDF.vb

---
+++ VB/13_Conversion/SVGToPDF.vb
@@ -0,0 +1,37 @@
+﻿Imports Spire.Pdf
+Imports Spire.Pdf.Annotations
+Imports Spire.Pdf.Graphics
+Imports System.ComponentModel
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace SVGToPDF
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a new PDF document.
+			Dim doc As New PdfDocument()
+
+			'Load the file from disk.
+			doc.LoadFromSvg("..\..\..\..\..\..\Data\template.svg")
+
+			'Save the document
+			Dim result As String = "SVgToPDF_out.pdf"
+			doc.SaveToFile(result)
+
+			'Launch the Pdf file
+			PDFDocumentViewer(result)
+		End Sub
+
+		Private Sub PDFDocumentViewer(ByVal filename As String)
+			Try
+				Process.Start(filename)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
