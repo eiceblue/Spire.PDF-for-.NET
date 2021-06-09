4ae77ebfa2a13973c0e1543467f9673628c972c1
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:30:48 +0800

    Create ToHTMLFilesSplittedByPages.vb

---
+++ VB/13_Conversion/ToHTMLFilesSplittedByPages.vb
@@ -0,0 +1,40 @@
+﻿Imports Spire.Pdf
+Imports System.ComponentModel
+Imports System.Text
+
+
+Namespace ToHTMLFilesSplittedByPages
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Pdf file
+			Dim file As String = "..\..\..\..\..\..\..\Data\SampleB_3.pdf"
+
+			'Open pdf document
+			Dim doc As New PdfDocument()
+			doc.LoadFromFile(file)
+
+			'Split to HTML file according to pages, here one page will convert to a HTML file.
+			doc.ConvertOptions.SetPdfToHtmlOptions(False, True, 1)
+
+			Dim output As String = "ToHTMLFilesSplittedByPages_out.html"
+
+			'Convert to html file
+			doc.SaveToFile(output, FileFormat.HTML)
+			doc.Close()
+
+			'Launch result file
+			PDFDocumentViewer(output)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
