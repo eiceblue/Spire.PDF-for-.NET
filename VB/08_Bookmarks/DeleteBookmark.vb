5dfc2d34023e857d3e8edfed664c4b5062095c40
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 16:50:22 +0800

    Create DeleteBookmark.vb

---
+++ VB/08_Bookmarks/DeleteBookmark.vb
@@ -0,0 +1,36 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Spire.Pdf
+
+Namespace DeleteBookmark
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Load a pdf document
+			Dim input As String = "..\..\..\..\..\..\Data\DeleteBookmark.pdf"
+			Dim doc As New PdfDocument()
+		doc.LoadFromFile(input)
+
+			'Delete the first bookmark
+			doc.Bookmarks.RemoveAt(0)
+
+			'Save the pdf document
+			Dim output As String = "DeleteBookmark.pdf"
+			doc.SaveToFile(output)
+
+			'Launch the file
+			PDFDocumentViewer(output)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+
+	End Class
+End Namespace
