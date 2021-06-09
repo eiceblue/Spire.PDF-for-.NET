1a574570ffc56044146a5e2c33a590ca9ed67cc8
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:41:59 +0800

    Create SetExpiryDate.vb

---
+++ VB/15_Document/SetExpiryDate.vb
@@ -0,0 +1,46 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Spire.Pdf
+Imports Spire.Pdf.Fields
+Imports Spire.Pdf.Annotations
+Imports Spire.Pdf.Graphics
+Imports Spire.Pdf.Actions
+Imports Spire.Pdf.General
+Imports Spire.Pdf.General.Find
+Namespace SetExpiryDate
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim input As String = "..\..\..\..\..\..\Data\PDFTemplate-Az.pdf"
+			Dim doc As New PdfDocument()
+
+			' Read a pdf file
+			doc.LoadFromFile(input)
+
+			Dim javaScript As String = "var rightNow = new Date();" & "var endDate = new Date('October 20, 2015 23:59:59');" & "if(rightNow.getTime() > endDate)" & "app.alert('This document has expired, please contact us for a new one.',1);" & "this.closeDoc();"
+
+			Dim js As New PdfJavaScriptAction(javaScript)
+
+			doc.AfterOpenAction = js
+
+
+			Dim result As String = "SetExpiryDate_out.pdf"
+
+			'Save the document
+			doc.SaveToFile(result)
+			'Launch the Pdf file
+			PDFDocumentViewer(result)
+		End Sub
+
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
