320dc619dbe465c940ffbe7a106ec306a9680fe8
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 16:50:25 +0800

    Create ExpandBookmarks.vb

---
+++ VB/08_Bookmarks/ExpandBookmarks.vb
@@ -0,0 +1,37 @@
+﻿Imports Spire.Pdf
+Imports System.ComponentModel
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace ExpandBookmarks
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a new PDF document.
+			Dim doc As New PdfDocument()
+
+			'Load the file from disk.
+			doc.LoadFromFile("..\..\..\..\..\..\Data\Template_Pdf_1.pdf")
+
+			'Set BookMarkExpandOrCollapse as true to expand the bookmarks.
+			doc.ViewerPreferences.BookMarkExpandOrCollapse = True
+
+			Dim result As String = "ExpandBookmarks_out.pdf"
+
+			'Save the document
+			doc.SaveToFile(result)
+			'Launch the Pdf file
+			PDFDocumentViewer(result)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal filename As String)
+			Try
+				Process.Start(filename)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
