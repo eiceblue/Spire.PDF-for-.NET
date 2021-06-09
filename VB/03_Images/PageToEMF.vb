372c33a30f686144eeac1867a81aa3d6f32a2ae5
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 16:44:25 +0800

    Create PageToEMF.vb

---
+++ VB/03_Images/PageToEMF.vb
@@ -0,0 +1,33 @@
+Imports System.Drawing.Imaging
+Imports Spire.Pdf
+Imports Spire.Pdf.Graphics
+
+Namespace PageToEMF
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Pdf file
+			Dim file As String = "..\..\..\..\..\..\Data\PageToImage.pdf"
+
+			'Open pdf document
+			Dim pdf As New PdfDocument()
+			pdf.LoadFromFile(file)
+
+			'Convert a particular page to emf
+			'Set page index and image name
+			Dim pageIndex As Integer = 1
+			Dim fileName As String = "PageToEMF.emf"
+			'Save page to image in matefile type
+			Using image As Image = pdf.SaveAsImage(pageIndex, PdfImageType.Metafile, 300, 300)
+				image.Save(fileName, ImageFormat.Emf)
+			End Using
+
+			pdf.Close()
+		End Sub
+
+	End Class
+End Namespace
