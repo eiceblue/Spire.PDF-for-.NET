302d45f638c90e77afe080a4cc9fb60d0f495198
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:27:17 +0800

    Create ChangeSecurityPermission.vb

---
+++ VB/11_SecurityAndSignatures/ChangeSecurityPermission.vb
@@ -0,0 +1,27 @@
+﻿Imports Spire.Pdf
+Imports Spire.Pdf.Security
+Imports System.ComponentModel
+Imports System.Text
+
+Namespace ChangeSecurityPermission
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create and load a pdf document
+			Dim pdf As New PdfDocument()
+			pdf.LoadFromFile("..\..\..\..\..\..\Data\ChangeSecurityPermission.pdf")
+
+			'Set an owner password, enable the permissions of Printing and Copying, set encryption level
+			pdf.Security.Encrypt("", "test  ", PdfPermissionsFlags.FillFields Or PdfPermissionsFlags.FullQualityPrint,PdfEncryptionKeySize.Key256Bit)
+
+			'Save and launch
+			pdf.SaveToFile("SecurityPermission.pdf")
+			Process.Start("SecurityPermission.pdf")
+
+		End Sub
+	End Class
+End Namespace
