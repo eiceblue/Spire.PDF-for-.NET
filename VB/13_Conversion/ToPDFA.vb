17cc6f7f059bf66d8b7697dca6ce4891cb537237
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:31:08 +0800

    Create ToPDFA.vb

---
+++ VB/13_Conversion/ToPDFA.vb
@@ -0,0 +1,32 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Spire.Pdf
+Imports Spire.Pdf.Conversion
+
+Namespace ToPDFA
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Pdf file
+			Dim input As String = "..\..\..\..\..\..\Data\ToPDFA.pdf"
+
+      Dim converter As PdfStandardsConverter = New PdfStandardsConverter(input)
+      Dim output As String = "ToPDFA-result.pdf"
+      converter.ToPdfA1B(output)
+
+			'Launch the result file.
+			PDFDocumentViewer(output)
+		End Sub
+
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
