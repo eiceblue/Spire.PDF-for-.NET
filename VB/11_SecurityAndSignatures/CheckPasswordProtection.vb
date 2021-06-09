8c45b115a8badc63ca0d63547e93462baaa68835
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:27:20 +0800

    Create CheckPasswordProtection.vb

---
+++ VB/11_SecurityAndSignatures/CheckPasswordProtection.vb
@@ -0,0 +1,17 @@
+﻿Imports Spire.Pdf
+
+Namespace CheckPasswordProtection
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Check whether the input pdf document is password protected.
+			Dim isProtected As Boolean =PdfDocument.IsPasswordProtected("..\..\..\..\..\..\Data\CheckPasswordProtection.pdf")
+			'Show the result by message box
+			MessageBox.Show("The pdf is " & (If(isProtected, "password ", "not password ")) & "protected!")
+		End Sub
+	End Class
+End Namespace
