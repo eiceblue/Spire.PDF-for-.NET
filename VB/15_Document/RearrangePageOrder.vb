98f36e6d029d46e8e183d9d35dd561535b96f5bb
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:41:43 +0800

    Create RearrangePageOrder.vb

---
+++ VB/15_Document/RearrangePageOrder.vb
@@ -0,0 +1,38 @@
+﻿Imports Spire.Pdf
+Imports System.ComponentModel
+Imports System.Text
+
+Namespace RearrangePageOrder
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a pdf document
+			Dim doc As New PdfDocument()
+
+			'Load from file
+			doc.LoadFromFile("..\..\..\..\..\..\Data\SampleB_3.pdf")
+
+			'Rearrange the page order
+			doc.Pages.ReArrange(New Integer() { 1, 0})
+
+			Dim result As String="RearrangePageOrder-result.pdf"
+			'Save to file
+			doc.SaveToFile(result, FileFormat.PDF)
+
+			'Launch the Pdf file
+			PDFDocumentViewer(result)
+
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+
+		End Sub
+	End Class
+End Namespace
