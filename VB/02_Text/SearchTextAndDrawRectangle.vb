28c267cd59dc4093db77743bb052967938c924d1
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 16:42:52 +0800

    Create SearchTextAndDrawRectangle.vb

---
+++ VB/02_Text/SearchTextAndDrawRectangle.vb
@@ -0,0 +1,50 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Spire.Pdf
+Imports Spire.Pdf.Fields
+Imports Spire.Pdf.Annotations
+Imports Spire.Pdf.Graphics
+Imports Spire.Pdf.Actions
+Imports Spire.Pdf.General
+Imports Spire.Pdf.General.Find
+Namespace SearchTextAndDrawRectangle
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim input As String = "..\..\..\..\..\..\Data\SearchReplaceTemplate.pdf"
+			Dim doc As New PdfDocument()
+
+			' Read a pdf file
+			doc.LoadFromFile(input)
+
+			' Get the first page of pdf file
+			Dim page As PdfPageBase = doc.Pages(0)
+
+			' Create PdfTextFindCollection object to find all the matched phrases
+			Dim collection As PdfTextFindCollection = page.FindText("Spire.PDF for .NET", TextFindParameter.IgnoreCase)
+
+			For Each find As PdfTextFind In collection.Finds
+				' Draw a rectangle with red pen
+				page.Canvas.DrawRectangle(New PdfPen(PdfBrushes.Red,0.9f), find.Bounds)
+			Next find
+
+			Dim result As String = "SearchTextAndDrawRectangle_out.pdf"
+
+			'Save the document
+			doc.SaveToFile(result)
+			'Launch the Pdf file
+			PDFDocumentViewer(result)
+		End Sub
+
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
