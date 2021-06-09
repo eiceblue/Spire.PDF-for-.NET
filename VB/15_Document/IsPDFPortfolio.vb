de4a5fdbb09ef7b6477f0abe9f94012a33b6ef6f
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:41:16 +0800

    Create IsPDFPortfolio.vb

---
+++ VB/15_Document/IsPDFPortfolio.vb
@@ -0,0 +1,28 @@
+﻿Imports Spire.Pdf
+Imports System.ComponentModel
+Imports System.Text
+
+Namespace IsPDFPortfolio
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a pdf document
+			Dim doc As New PdfDocument()
+
+			'Load from file
+			doc.LoadFromFile("..\..\..\..\..\..\Data\TextBoxSampleB_1.pdf")
+
+			'Judge whether the document is portfolio or not.
+			Dim value As Boolean = doc.IsPortfolio
+			If value Then
+				MessageBox.Show("The document is portfolio")
+			Else
+				MessageBox.Show("The document is not portfolio")
+			End If
+		End Sub
+	End Class
+End Namespace
