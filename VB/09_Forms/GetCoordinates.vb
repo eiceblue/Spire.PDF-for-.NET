827c0414c98b30ace898b12d83a2ecf354793b45
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 16:52:25 +0800

    Create GetCoordinates.vb

---
+++ VB/09_Forms/GetCoordinates.vb
@@ -0,0 +1,33 @@
+﻿Imports Spire.Pdf
+Imports Spire.Pdf.Widget
+Imports System.ComponentModel
+Imports System.Text
+
+Namespace GetCoordinates
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a pdf document
+			Dim doc As New PdfDocument()
+
+			'Load from file
+			doc.LoadFromFile("..\..\..\..\..\..\Data\TextBoxSampleB.pdf")
+
+			'Get form fields
+			Dim formWidget As PdfFormWidget = TryCast(doc.Form, PdfFormWidget)
+
+			'Get textbox
+			Dim textbox As PdfTextBoxFieldWidget = TryCast(formWidget.FieldsWidget("Text1"), PdfTextBoxFieldWidget)
+
+			'Get the location of the textbox
+			Dim location As PointF = textbox.Location
+
+			MessageBox.Show("The location of the field named " & textbox.Name & " is" & vbLf & " X:" & location.X & "  Y:" & location.Y)
+
+		End Sub
+	End Class
+End Namespace
