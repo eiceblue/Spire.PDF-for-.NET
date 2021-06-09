a2498743023b6e4f7ee706ee1fc4fcd6432abc1d
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:40:37 +0800

    Create DeleteLayer.vb

---
+++ VB/15_Document/DeleteLayer.vb
@@ -0,0 +1,33 @@
+﻿Imports Spire.Pdf
+Imports System.ComponentModel
+Imports System.Text
+
+Namespace DeleteLayer
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Load the document from disk
+			Dim doc As New PdfDocument()
+			doc.LoadFromFile("..\..\..\..\..\..\Data\DeleteLayer.pdf")
+
+			'Delete the "red line" layer
+			doc.Layers.RemoveLayer("red line")
+
+			'Save the document
+			doc.SaveToFile("Output.pdf")
+
+			'View the Pdf file
+			PDFDocumentViewer("Output.pdf")
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
