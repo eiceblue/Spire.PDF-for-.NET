2adaaf9e1d55815c55b280ef445229bd6911320b
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:32:21 +0800

    Create GetNumberOfPages.vb

---
+++ VB/14_Page/GetNumberOfPages.vb
@@ -0,0 +1,33 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Spire.Pdf
+Imports Spire.Pdf.Widget
+
+Namespace GetNumberOfPages
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim dialog As New OpenFileDialog()
+			dialog.Filter = "PDF document (*.pdf)|*.pdf"
+			Dim result As DialogResult = dialog.ShowDialog()
+			If result = DialogResult.OK Then
+				Try
+					Dim pdfFile As String = dialog.FileName
+
+					'Open a pdf document and get its page count
+					Using pdf As New PdfDocument()
+						pdf.LoadFromFile(pdfFile)
+						Dim count As Integer = pdf.Pages.Count
+						MessageBox.Show("The page count of the pdf document is " & count)
+					End Using
+				Catch exe As Exception
+					MessageBox.Show(exe.Message, "Spire.Pdf Demo", MessageBoxButtons.OK, MessageBoxIcon.Error)
+				End Try
+			End If
+		End Sub
+	End Class
+End Namespace
