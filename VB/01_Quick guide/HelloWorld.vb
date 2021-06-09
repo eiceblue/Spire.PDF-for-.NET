2628872f9602c1fd538483271794ab6b6c1ac118
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 16:40:51 +0800

    Update HelloWorld.vb

--- VB/01_Quick guide/HelloWorld.vb
+++ VB/01_Quick guide/HelloWorld.vb
@@ -10,22 +10,23 @@ Namespace HelloWorld
 		End Sub
 
 		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			'Create a pdf document.
+			'Create a pdf document
 			Dim doc As New PdfDocument()
 
-			' Create one page
+			'Create one page
 			Dim page As PdfPageBase = doc.Pages.Add()
 
 			'Draw the text
 			page.Canvas.DrawString("Hello, World!", New PdfFont(PdfFontFamily.Helvetica, 30f), New PdfSolidBrush(Color.Black), 10, 10)
 
-			'Save pdf file.
-			doc.SaveToFile("HelloWorld.pdf")
+			Dim result As String = "HelloWorld.pdf"
+
+			'Save the document
+			doc.SaveToFile(result)
 			doc.Close()
 
-			'Launching the Pdf file.
-			PDFDocumentViewer("HelloWorld.pdf")
-
+			'Launch the Pdf file
+			PDFDocumentViewer(result)
 		End Sub
 
 		Private Sub PDFDocumentViewer(ByVal fileName As String)
