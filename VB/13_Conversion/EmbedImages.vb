2467f1b866248f260f5700e24e15da42523b4c9b
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:30:06 +0800

    Create EmbedImages.vb

---
+++ VB/13_Conversion/EmbedImages.vb
@@ -0,0 +1,39 @@
+﻿Imports Spire.Pdf
+Imports System.ComponentModel
+Imports System.Text
+
+Namespace EmbedImages
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Pdf file
+			Dim file As String = "..\..\..\..\..\..\..\Data\EmbedImagesInHTML.pdf"
+
+			'Open pdf document
+			Dim doc As New PdfDocument()
+			doc.LoadFromFile(file)
+
+			'Set the convertion option to embed image in html
+			doc.ConvertOptions.SetPdfToHtmlOptions(True, True)
+
+			Dim result As String = "ToHTMLWithEmbedImages_out.html"
+
+			'Convert to html file
+			doc.SaveToFile(result, FileFormat.HTML)
+			doc.Close()
+
+			'Launch the reuslt file
+			PDFDocumentViewer(result)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
