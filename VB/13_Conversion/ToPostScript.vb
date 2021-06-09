1d846e892b67c905853babccf68a70246ea00a57
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:31:22 +0800

    Create ToPostScript.vb

---
+++ VB/13_Conversion/ToPostScript.vb
@@ -0,0 +1,36 @@
+﻿Imports System.ComponentModel
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports Spire.Pdf
+
+Namespace ToPostScript
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim input As String = "..\..\..\..\..\..\Data\ToPostScript.pdf"
+
+			'Load a PDF document
+			Dim document As New PdfDocument()
+			document.LoadFromFile(input)
+
+			'Save to PostScript
+			Dim output As String = "toPostScript_result.ps"
+			document.SaveToFile(output, FileFormat.POSTSCRIPT)
+
+			'Launch the file
+			PDFDocumentViewer(output)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+
+		End Sub
+	End Class
+End Namespace
