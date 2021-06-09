52a49e84a0e3a828793d461e40b2e87bbc973468
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 16:49:50 +0800

    Create DeleteAllAttachments.vb

---
+++ VB/07_Attachments/DeleteAllAttachments.vb
@@ -0,0 +1,42 @@
+﻿Imports System.ComponentModel
+Imports System.Text
+Imports Spire.Pdf
+Imports Spire.Pdf.Attachments
+Imports System.IO
+
+Namespace DeleteAllAttachments
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Dim input As String = "..\..\..\..\..\..\Data\DeleteAllAttachments.pdf"
+
+			'Open pdf document
+			Dim doc As New PdfDocument()
+		doc.LoadFromFile(input)
+
+			'Get all attachments
+			Dim attachments As PdfAttachmentCollection = doc.Attachments
+
+			'Delete all attachments
+			attachments.Clear()
+
+			Dim output As String = "DeleteAllAttachments.pdf"
+
+			'Save pdf document
+			doc.SaveToFile(output)
+
+			'Launch the Pdf file
+			PDFDocumentViewer(output)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
