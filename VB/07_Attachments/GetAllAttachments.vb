f635349d032b1df97aae654bd4c1e7173924db96
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 16:49:53 +0800

    Create GetAllAttachments.vb

---
+++ VB/07_Attachments/GetAllAttachments.vb
@@ -0,0 +1,31 @@
+﻿Imports Spire.Pdf
+Imports Spire.Pdf.Attachments
+Imports System.ComponentModel
+Imports System.IO
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace GetAllAttachments
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a new PDF document.
+			Dim pdf As New PdfDocument()
+
+			'Load the file from disk.
+			pdf.LoadFromFile("..\..\..\..\..\..\Data\Template_Pdf_2.pdf")
+
+			'Get a collection of attachments on the PDF document.
+			Dim collection As PdfAttachmentCollection = pdf.Attachments
+
+			'Save all the attachments to the files.
+			For i As Integer = 0 To collection.Count - 1
+				File.WriteAllBytes(collection(i).FileName, collection(i).Data)
+			Next i
+		End Sub
+	End Class
+End Namespace
