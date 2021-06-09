adc66ed71a111984b92e4d356b8d54c8a2674280
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:29:45 +0800

    Create RemoveOpenAction.vb

---
+++ VB/12_LinksAndActions/RemoveOpenAction.vb
@@ -0,0 +1,41 @@
+﻿Imports Spire.Pdf
+Imports Spire.Pdf.Actions
+Imports Spire.Pdf.Annotations
+Imports Spire.Pdf.Graphics
+Imports System.ComponentModel
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace RemoveOpenAction
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a pdf document
+			Dim document As New PdfDocument()
+
+			'Load an existing pdf from disk
+			document.LoadFromFile("..\..\..\..\..\..\Data\OpenAction.pdf")
+
+			'Remove action
+			document.AfterOpenAction = Nothing
+
+			Dim result As String = "Output.pdf"
+			'Save the document
+			document.SaveToFile(result)
+
+			'Launch the Pdf file
+			PDFDocumentViewer(result)
+		End Sub
+
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
