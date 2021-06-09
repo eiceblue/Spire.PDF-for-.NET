bd161acfaed9969f2b569244e12f585a31905f15
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:30:04 +0800

    Create ConvertToGrayPdf.vb

---
+++ VB/13_Conversion/ConvertToGrayPdf.vb
@@ -0,0 +1,27 @@
+﻿Imports Spire.Pdf.Conversion
+
+Namespace ConvertToGrayPdf
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Output file path
+			Dim output As String = "ConvertToGrayPdf-result.pdf"
+			'Create a PdfGrayConverter with an pdf file
+			Dim converter As New PdfGrayConverter("..\..\..\..\..\..\Data\ConvertToGrayPdf.pdf")
+			'Convert the file to gray pdf
+			converter.ToGrayPdf(output)
+			'Launch the Pdf file
+			PDFDocumentViewer(output)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
