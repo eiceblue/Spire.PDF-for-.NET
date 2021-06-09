f4d3c9244f032f57b23b08ad1f0bfd781cf9acef
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:32:34 +0800

    Create InsertEmptyPage.vb

---
+++ VB/14_Page/InsertEmptyPage.vb
@@ -0,0 +1,42 @@
+﻿Imports Spire.Pdf
+Imports Spire.Pdf.Actions
+Imports Spire.Pdf.General
+Imports Spire.Pdf.Graphics
+Imports System.ComponentModel
+Imports System.Drawing.Printing
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace InsertEmptyPage
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			'Create a pdf document
+			Dim doc As New PdfDocument()
+
+			'Load an existing pdf from disk
+			doc.LoadFromFile("..\..\..\..\..\..\Data\Sample.pdf")
+
+			'insert a blank page as the second page
+			doc.Pages.Insert(1)
+
+			Dim result As String = "InsertEmptyPage_out.pdf"
+
+			'Save the document
+			doc.SaveToFile(result)
+
+			'Launch the Pdf file
+			PDFDocumentViewer(result)
+		End Sub
+		Private Sub PDFDocumentViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+	End Class
+End Namespace
