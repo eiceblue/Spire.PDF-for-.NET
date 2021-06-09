a7043e6506aeec4d09875e04ed3fbc7fa604f270
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2021-06-09 17:30:25 +0800

    Create PdfToExcelOptions.vb

---
+++ VB/13_Conversion/PdfToExcelOptions.vb
@@ -0,0 +1,34 @@
+﻿Imports Spire.Pdf
+
+Namespace PdfToExcelOptions
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub btnRun_Click(ByVal sender As Object, ByVal e As EventArgs) Handles label1.Click, button1.Click, pictureBox1.Click
+			Dim pdf As New PdfDocument()
+			Dim PDFFile As String = "..\..\..\..\..\..\Data\PdfToXlsxOptions.pdf"
+			pdf.LoadFromFile(PDFFile)
+
+			pdf.ConvertOptions.SetPdfToXlsxOptions(PdfToXlsxLayout.Line)
+			Dim ExcelFile As String = "PdfToXlsxOptions_results.xlsx"
+			'Save the pdf file to excel
+			pdf.SaveToFile(ExcelFile, FileFormat.XLSX)
+			'View the excel document
+			FileViewer(ExcelFile)
+		End Sub
+
+		Private Sub FileViewer(ByVal fileName As String)
+			Try
+				Process.Start(fileName)
+			Catch
+			End Try
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+
+		End Sub
+	End Class
+End Namespace
